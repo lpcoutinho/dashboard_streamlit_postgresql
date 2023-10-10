e5b6e9059dd0d0bed99e3a5f012be8792de187ae
Luiz Paulo Coutinho <coutinholps@gmail.com>
2023-10-10 18:41:32 -0300

    add sidebar

--- home.py
+++ home.py
@@ -26,4 +26,31 @@ df = pd.DataFrame(
         "Rating",
     ],
 )
-df
+
+# side bar
+# logo
+st.sidebar.image("resources/img/logo1.png",caption="Online Analytics")
+
+# switcher
+st.sidebar.header("Filtre os dados")
+region=st.sidebar.multiselect(
+    "Região",
+     options=df["Region"].unique(),
+     default=df["Region"].unique(),
+)
+location=st.sidebar.multiselect(
+    "Localização",
+     options=df["Location"].unique(),
+     default=df["Location"].unique(),
+)
+construction=st.sidebar.multiselect(
+    "Tipo de construção",
+     options=df["Construction"].unique(),
+     default=df["Construction"].unique(),
+)
+
+df_selection=df.query(
+    "Region==@region & Location==@location & Construction ==@construction"
+)
+
+df_selection
